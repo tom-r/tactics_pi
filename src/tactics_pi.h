35834f9ced07e7a9dd8c007e5cf3589ca78bbe62
tom <tom>
2017-07-04 20:50:31 +0200

    patch 6

--- src/tactics_pi.h
+++ src/tactics_pi.h
@@ -36,7 +36,7 @@
 
 #define     PLUGIN_VERSION_MAJOR    1
 #define     PLUGIN_VERSION_MINOR    0
-#define     PLUGIN_VERSION_PATCH    5
+#define     PLUGIN_VERSION_PATCH    6
 
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    12
