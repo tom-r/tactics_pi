3d0f1bd14133f12d0e416b6aed3c4e49bb1cd4f3
Dave <bdbcat@yahoo.com>
2023-09-28 17:59:08 -0400

    Correct Android build #2

--- src/performance.cpp
+++ src/performance.cpp
@@ -365,6 +365,9 @@ Polar::Polar(TacticsInstrument_PerformanceSingle* parent)
 	wxString stdPath = std_path.GetUserConfigDir();   // should be ~/Library/Preferences
 	stdPath += s + _T("opencpn");
 #endif
+#ifdef __OCPN__ANDROID__
+    wxString stdPath = std_path.GetUserDataDir();
+#endif
 
 	wxString basePath = stdPath + s + _T("plugins") + s + _T("tactics_pi") + s + _T("data") + s;
 	logbookDataPath = basePath;
